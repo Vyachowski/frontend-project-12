74f0f8ed5fa50cf8f40e79b4a6bf4340f23499f1
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-02-14 21:16:01 +0200

    feat: add login form draft

--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -1,5 +1,22 @@
+import {Field, Form, Formik} from "formik";
+
 export default function LoginPage() {
   return (
-    <div>Login Page</div>
-  )
+    <div className="LoginForm">
+      <h1>Sign In</h1>
+      <Formik
+        initialValues={{ nickname: "", password: "" }}
+        onSubmit={async (values) => {
+          await new Promise((resolve) => setTimeout(resolve, 500));
+          alert(JSON.stringify(values, null, 2));
+        }}
+      >
+        <Form>
+          <Field name="name" type="text" />
+          <Field name="password" type="password" />
+          <button type="submit">Submit</button>
+        </Form>
+      </Formik>
+    </div>
+  );
 }
