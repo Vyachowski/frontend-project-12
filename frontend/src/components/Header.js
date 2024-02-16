4d3ca5d3e3ea4a2b34007c627b9e6748973fc711
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-02-16 20:03:34 +0200

    feat: add chat and header components

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
+      <div className="container">
+        <a className="navbar-brand" href="/">Hexlet Chat</a>
+        <button type="button" className="btn btn-primary">Выйти</button>
+      </div>
+    </nav>
+  )
+}
+
+export default Header;
