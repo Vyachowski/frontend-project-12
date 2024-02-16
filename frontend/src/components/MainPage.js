52388cf2795d242a20b680cb435035a4fe9eb464
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-02-16 12:06:03 +0200

    feat: add redirection to login page from main

--- frontend/src/components/MainPage.js
+++ frontend/src/components/MainPage.js
@@ -1,5 +1,17 @@
+import {useNavigate} from "react-router-dom";
+import {useEffect} from "react";
+
 export default function MainPage() {
+  const navigate = useNavigate();
+  const isAuthenticated = localStorage.getItem('authToken');
+
+  useEffect(() => {
+    !isAuthenticated && navigate('login');
+  }, [isAuthenticated, navigate]);
+
   return (
-    <div>Main Page</div>
-  )
+    !isAuthenticated
+      ? <div>Redirect to login page...</div>
+      : <div>CHAT WILL BE HERE SOON!</div>
+  );
 }
