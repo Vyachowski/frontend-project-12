7555c91e18f6837f5ad2b952f7f0af00c6776004
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-02-17 12:42:34 +0200

    feat: add chat markup

--- frontend/src/components/Chat.js
+++ frontend/src/components/Chat.js
@@ -1,4 +1,4 @@
-import {Button, Col, Container, Row} from "react-bootstrap";
+import {Button, Col, Container, Form, ListGroup, ListGroupItem, Row} from "react-bootstrap";
 
 const Chat = () => {
   return (
@@ -17,34 +17,35 @@ const Chat = () => {
               <span className="visually-hidden">+</span>
             </Button>
           </div>
-          <ul id="channels-box" className="nav flex-column nav-pills nav-fill px-2 mb-3 overflow-auto h-100 d-block">
-            <li className="nav-item w-100">
-              <button type="button" className="w-100 rounded-0 text-start btn btn-secondary">
+          <ListGroup id="channels-box" className="nav flex-column nav-pills nav-fill px-2 mb-3 overflow-auto h-100 d-block" defaultActiveKey="#link1" as='ul'>
+            <ListGroupItem className="nav-item p-0 w-100" as='li'>
+              <Button type='button' variant={'secondary'} className={'w-100 rounded-0 text-start'}>
                 <span className="me-1">#</span>general
-              </button>
-            </li>
-            <li className="nav-item w-100">
-              <button type="button" className="w-100 rounded-0 text-start btn"><span className="me-1">#</span>random
-              </button>
-            </li>
-          </ul>
+              </Button>
+            </ListGroupItem>
+          </ListGroup>
         </Col>
         <Col className="col p-0 h-100">
           <div className="d-flex flex-column h-100">
-            <div className="bg-light mb-4 p-3 shadow-sm small"><p className="m-0"><b># general</b></p><span
-              className="text-muted">0 сообщений</span></div>
-            <div id="messages-box" className="chat-messages overflow-auto px-5 "></div>
+            <div className="bg-light mb-4 p-3 shadow-sm small">
+              <p className="m-0">
+                <b># general</b>
+              </p>
+              <span className="text-muted">0 сообщений</span>
+            </div>
+            <div id="messages-box" className="chat-messages overflow-auto px-5"></div>
             <div className="mt-auto px-5 py-3">
-              <form noValidate="" className="py-1 border rounded-2">
-                <div className="input-group has-validation">
-                  <input name="body" aria-label="Новое сообщение" placeholder="Введите сообщение..." className="border-0 p-0 ps-2 form-control"/>
-                  <button type="submit" disabled="" className="btn btn-group-vertical">
+              <Form noValidate="" className="py-1 border rounded-2">
+                <Form.Group className="d-flex has-validation">
+                  <Form.Control className="border-0 p-0 ps-2 form-control" name="body" aria-label="Новое сообщение" placeholder="Введите сообщение..."/>
+                  <Button type="submit" className={'border-0 lh-1'} variant={'outline-secondary'} disabled>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="20" height="20" fill="currentColor">
                       <path fillRule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"></path>
                     </svg>
-                    <span className="visually-hidden">Отправить</span></button>
-                </div>
-              </form>
+                    <span className="visually-hidden">Отправить</span>
+                  </Button>
+                </Form.Group>
+              </Form>
             </div>
           </div>
         </Col>
