07a585dfa05f8fec9d8cb0fb01cd72db7861062e
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-02-18 12:02:02 +0200

    feat: add data fetching from server

--- frontend/src/components/MainPage.js
+++ frontend/src/components/MainPage.js
@@ -2,17 +2,38 @@ import {useNavigate} from "react-router-dom";
 import {useEffect} from "react";
 import Header from "./Header.js";
 import Chat from "./Chat.js";
+import axios from "axios";
 
 const MainPage = () => {
   const navigate = useNavigate();
-  const isAuthenticated = localStorage.getItem('token');
+  const token = localStorage.getItem('token');
 
   useEffect(() => {
-    !isAuthenticated && navigate('login');
-  }, [isAuthenticated, navigate]);
+    const syncChannelsAndMessages = async () => {
+      if (!token) {
+        navigate('login');
+      }
+
+      const { data: channels } = await axios
+        .get('/api/v1/channels', {
+          headers: {"Authorization" : `Bearer ${token}`}
+        })
+
+      console.log(channels);
+
+      const { data: messages } = await axios
+        .get('/api/v1/messages', {
+          headers: {"Authorization" : `Bearer ${token}`}
+        })
+
+      console.log(messages);
+    }
+
+    syncChannelsAndMessages();
+  }, [token, navigate]);
 
   return (
-    !isAuthenticated
+    !token
       ? <div>Redirect to login page...</div>
       : (
         <div className={'d-flex flex-column h-100'}>
