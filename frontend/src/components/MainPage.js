72117a1c1d46ef24d01fec343c8ceda79c80b954
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-02-19 20:56:41 +0200

    refactor: delete previous data fetching

--- frontend/src/components/MainPage.js
+++ frontend/src/components/MainPage.js
@@ -2,34 +2,15 @@ import {useNavigate} from "react-router-dom";
 import {useEffect} from "react";
 import Header from "./Header.js";
 import Chat from "./Chat.js";
-import axios from "axios";
 
 const MainPage = () => {
   const navigate = useNavigate();
   const token = localStorage.getItem('token');
 
   useEffect(() => {
-    const syncChannelsAndMessages = async () => {
-      if (!token) {
-        navigate('/login');
-      }
-
-      const { data: channels } = await axios
-        .get('/api/v1/channels', {
-          headers: {"Authorization" : `Bearer ${token}`}
-        })
-
-      console.log(channels);
-
-      const { data: messages } = await axios
-        .get('/api/v1/messages', {
-          headers: {"Authorization" : `Bearer ${token}`}
-        })
-
-      console.log(messages);
+    if (!token) {
+      navigate('/login');
     }
-
-    syncChannelsAndMessages();
   }, [token, navigate]);
 
   return (
