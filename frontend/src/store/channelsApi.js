b4392567bbf946fd05c989de226704897de29ed3
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-02-21 17:33:01 +0200

    refactor: fix import for API

--- frontend/src/store/channelsApi.js
+++ frontend/src/store/channelsApi.js
@@ -1,8 +1,19 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const channelsApi = createApi({
   reducerPath: 'channels',
-  baseQuery: fetchBaseQuery({baseUrl: '/api/v1/channels'}),
+  baseQuery: fetchBaseQuery({
+    baseUrl: '/api/v1/channels',
+    prepareHeaders: (headers) => {
+      const token = localStorage.getItem('token');
+
+      if (token) {
+        headers.set('authorization', `Bearer ${token}`)
+      }
+
+      return headers
+    },
+    }),
   endpoints: (builder) => ({
     getChannels: builder.query({
       query: () => '',
@@ -31,5 +42,6 @@ export const channelsApi = createApi({
 export const {
   useGetChannelsQuery,
   useAddChannelMutation,
+  useRenameChannelMutation,
   useRemoveChannelMutation,
 } = channelsApi;
