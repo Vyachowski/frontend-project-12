601e11ba660c6c8d9d42b1be8334948aaabd14e0
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-02-25 14:41:03 +0200

    refactor: delete excess code

--- frontend/src/components/Messages.js
+++ frontend/src/components/Messages.js
@@ -1,9 +1,7 @@
 import { useSelector } from "react-redux";
-import { useEffect } from "react";
 import { Col } from "react-bootstrap";
 
 import MessagePanel from "./MessagePanel";
-import { socket } from "./Chat";
 
 const Messages = () => {
   const channels = useSelector((state) => Object.values(state.channels.entities));
@@ -12,18 +10,12 @@ const Messages = () => {
   const activeChannelId = useSelector(state => state.ui.activeChannelId);
   const activeChannel = channels.find((channel) => channel.id === activeChannelId);
 
-  useEffect(()=> {
-    socket.on('newMessage', (message) => {
-      console.log(message);
-    });
-  })
-
   return (
     <Col className="col p-0 h-100">
       <div className="d-flex flex-column h-100">
         <div className="bg-light mb-4 p-3 shadow-sm small">
           <p className="m-0">
-            <b># {activeChannel.name}</b>
+            <b># {activeChannel?.name}</b>
           </p>
           <span className="text-muted">0 сообщений</span>
         </div>
