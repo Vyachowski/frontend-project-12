7439fe69925c7a64cae8eff4830208a6d882743a
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-02-25 13:49:09 +0200

    refactor: separate component logic from chat

---
+++ frontend/src/components/Messages.js
@@ -0,0 +1,43 @@
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { Col } from "react-bootstrap";
+
+import MessagePanel from "./MessagePanel";
+import { socket } from "./Chat";
+
+const Messages = () => {
+  const messages = useSelector((state) => Object.values(state.messages.entities)) || [];
+  const activeChannelId = useSelector(state => state.ui.activeChannelId);
+
+  useEffect(()=> {
+    socket.on('newMessage', (message) => {
+      console.log(message);
+    });
+  })
+
+  return (
+    <Col className="col p-0 h-100">
+      <div className="d-flex flex-column h-100">
+        <div className="bg-light mb-4 p-3 shadow-sm small">
+          <p className="m-0">
+            <b># general</b>
+          </p>
+          <span className="text-muted">0 сообщений</span>
+        </div>
+        <div id="messages-box" className="chat-messages overflow-auto px-5">
+          {messages
+            .filter((message) => message.channelId === activeChannelId)
+            .map((message) => (
+              <p className={'text-break mb-2'} key={message.id}>
+                <strong>{message.username}</strong>
+                : {message.body}
+              </p>
+            ))}
+        </div>
+        <MessagePanel />
+      </div>
+    </Col>
+  )
+}
+
+export default Messages;
