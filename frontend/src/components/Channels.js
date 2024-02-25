4083f25e72fb535e319eadd570831b02cc70b4bd
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-02-25 14:44:16 +0200

    feat: add onclick attribute

--- frontend/src/components/Channels.js
+++ frontend/src/components/Channels.js
@@ -12,7 +12,7 @@ const Channels = () => {
     <Col className="col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex">
       <div className="d-flex mt-1 justify-content-between mb-2 ps-4 pe-2 p-4">
         <b>Каналы</b>
-        <Button type="button" variant="link" className="p-0 lh-1">
+        <Button type="button" variant="link" className="p-0 lh-1" onClick={null}>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="20" height="20" fill="currentColor">
             <path
               d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"></path>
