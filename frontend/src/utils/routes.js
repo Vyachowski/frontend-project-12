d3f7006b7d0fd607dc1c01c6e7619ae804a2bc20
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-02-25 11:40:48 +0200

    feat: add separate auth config variable

--- frontend/src/utils/routes.js
+++ frontend/src/utils/routes.js
@@ -1,10 +1,15 @@
-const baseUrl = '/api/v1'
+import {getToken} from "../store/authSlice";
+
+const baseUrl = '/api/v1';
+const authConfig = { headers: { 'Authorization': `Bearer ${getToken()}` }};
+
 const getMessageUrl = () => `${baseUrl}/messages`;
 const getUserUrl = (option) => option === 'signup' ? `${baseUrl}/signup` : `${baseUrl}/login`;
-const getChannelUrl = (id) => id ? `${baseUrl}/channels/${id}` : `${baseUrl}/channels`;
+const getChannelsUrl = (id) => id ? `${baseUrl}/channels/${id}` : `${baseUrl}/channels`;
 
 export {
+  authConfig,
   getMessageUrl,
   getUserUrl,
-  getChannelUrl,
+  getChannelsUrl,
 };
