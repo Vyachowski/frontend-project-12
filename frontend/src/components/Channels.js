9cf8d156c1750e579af8f9add99971ef0930bbc7
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-02-26 09:35:29 +0200

    feat: add logic for modal windows rendering

--- frontend/src/components/Channels.js
+++ frontend/src/components/Channels.js
@@ -1,21 +1,35 @@
 import { Button, Col, ListGroup, ListGroupItem } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 
-import { setActiveChannel } from "../store/uiSlice";
+import { setActiveChannel, setChannelModal } from "../store/uiSlice";
 
-import ChannelModal from "./ChannelModal";
+import AddChannelModal from "./AddChannelModal";
 
 const Channels = () => {
   const dispatch = useDispatch();
 
   const channels = useSelector((state) => Object.values(state.channels.entities));
   const activeChannelId = useSelector(state => state.ui.activeChannelId);
+  const showChannelModal = useSelector(state => state.ui.showChannelModal);
+  const channelModalType = useSelector(state => state.ui.channelModalType);
+
+  const renderModal= () => {
+    switch (channelModalType) {
+      // case 'RenameChannel':
+      //   return <RenameChannelModal />
+      // case 'RemoveChannel':
+      //   return <RemoveChannelModal />
+      default:
+        console.log(channelModalType)
+        return <AddChannelModal />
+    }
+  }
 
   return (
     <Col className="col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex">
       <div className="d-flex mt-1 justify-content-between mb-2 ps-4 pe-2 p-4">
         <b>Каналы</b>
-        <Button type="button" variant="link" className="p-0 lh-1" onClick={null}>
+        <Button type="button" variant="link" className="p-0 lh-1" onClick={() => dispatch(setChannelModal({ showChannelModal: true }))}>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="20" height="20" fill="currentColor">
             <path
               d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"></path>
@@ -39,7 +53,7 @@ const Channels = () => {
           </ListGroupItem>
         ))}
       </ListGroup>
-      <ChannelModal />
+      {showChannelModal && renderModal()}
     </Col>
   )
 }
