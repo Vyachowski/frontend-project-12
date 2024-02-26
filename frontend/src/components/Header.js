f3be9c93fbb514faafd7727d73f83680960f521c
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-02-26 11:20:20 +0200

    feat: add logout logic

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,22 @@
+import { Button } from "react-bootstrap";
+import { useDispatch } from "react-redux";
+import { logout } from "../store/authSlice";
+import { useNavigate } from "react-router-dom";
+
 const Header = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    dispatch(logout());
+    navigate('/login');
+  };
+
   return (
     <nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
       <div className="container">
         <a className="navbar-brand" href="/">Hexlet Chat</a>
-        <button type="button" className="btn btn-primary">Выйти</button>
+        <Button type="button" className="btn btn-primary" onClick={handleLogout}>Выйти</Button>
       </div>
     </nav>
   )
