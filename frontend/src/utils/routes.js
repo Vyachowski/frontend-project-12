21ecbe0f99f95b838d0bacae90d24911851d6018
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-02-26 10:30:56 +0200

    refactor: add new export

--- frontend/src/utils/routes.js
+++ frontend/src/utils/routes.js
@@ -5,9 +5,12 @@ const getMessageUrl = () => `${baseUrl}/messages`;
 const getUserUrl = (option) => option === 'signup' ? `${baseUrl}/signup` : `${baseUrl}/login`;
 const getChannelsUrl = (id) => id ? `${baseUrl}/channels/${id}` : `${baseUrl}/channels`;
 
+const getUsername = () => localStorage.getItem('username');
+
 export {
   authConfig,
   getMessageUrl,
   getUserUrl,
   getChannelsUrl,
+  getUsername,
 };
