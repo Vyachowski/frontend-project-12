024050e262e3014b1515f9ab6a041d1e0a3d8ecf
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-02-26 12:41:04 +0200

    feat: add config function

--- frontend/src/utils/routes.js
+++ frontend/src/utils/routes.js
@@ -1,18 +1,14 @@
 const baseUrl = '/api/v1';
-const authConfig = { headers: { 'Authorization': `Bearer ${localStorage.getItem('token')}` }};
 
 const getMessageUrl = () => `${baseUrl}/messages`;
 const getUserUrl = (option) => option === 'signup' ? `${baseUrl}/signup` : `${baseUrl}/login`;
 const getChannelsUrl = (id) => id ? `${baseUrl}/channels/${id}` : `${baseUrl}/channels`;
 
-const getUsername = () => localStorage.getItem('username');
-const getToken = () => localStorage.getItem('token');
+const getAuthConfig = (token) => ({ headers: { 'Authorization': `Bearer ${token}` }});
 
 export {
-  authConfig,
   getMessageUrl,
   getUserUrl,
   getChannelsUrl,
-  getUsername,
-  getToken,
+  getAuthConfig,
 };
