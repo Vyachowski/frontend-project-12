6ad82b118a51573b583ee85cb6178ddc3aa8d761
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-02-26 11:22:07 +0200

    feat: add isAuthenticated state

--- frontend/src/store/authSlice.js
+++ frontend/src/store/authSlice.js
@@ -1,25 +1,77 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
-const initialState = {};
+import { authConfig, getChannelsUrl, getToken, getUsername, getUserUrl } from "../utils/routes";
+
+export const login = createAsyncThunk(
+  'auth/login',
+  async (credentials) => {
+    const response = await axios.post(getUserUrl(), credentials, authConfig);
+    return response.data;
+  }
+);
+
+export const signup = createAsyncThunk(
+  'auth/signup',
+  async (credentials) => {
+    const response = await axios.post(getChannelsUrl(), credentials, authConfig);
+    return response.data;
+  }
+);
+
+const initialState = {
+  isAuthenticated: getUsername() && getToken(),
+};
 
 const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {
-    login: (state, action) => {
-      const { token, username } = action.payload;
-      localStorage.setItem('token', token);
-      localStorage.setItem('username', username);
-    },
-    logout: () => {
+    logout: (state) => {
       localStorage.removeItem('token');
       localStorage.removeItem('username');
+
+      state.isAuthenticated = false;
     }
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(login.pending, (state) => {
+        state.loadingStatus = 'loading';
+        state.error = null;
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        const { token, username } = action.payload;
+
+        localStorage.setItem('token', token);
+        localStorage.setItem('username', username);
+
+        state.isAuthenticated = true;
+        state.loadingStatus = 'idle';
+        state.error = null;
+      })
+      .addCase(login.rejected, (state) => {
+        state.loadingStatus = 'error';
+        state.error = null;
+      })
+      .addCase(signup.pending, (state) => {
+        state.loadingStatus = 'loading';
+        state.error = null;
+      })
+      .addCase(signup.fulfilled, (state, action) => {
+        const { token, username } = action.payload;
+        localStorage.setItem('token', token);
+        localStorage.setItem('username', username);
+
+        state.loadingStatus = 'idle';
+        state.error = null;
+      })
+      .addCase(signup.rejected, (state) => {
+        state.loadingStatus = 'error';
+        state.error = null;
+      })
   }
 });
 
-export const getToken = () => localStorage.getItem('token');
-export const getUsername = () => localStorage.getItem('username');
-
-export const { login, logout } = authSlice.actions;
+export const { logout } = authSlice.actions;
 export default authSlice.reducer;
