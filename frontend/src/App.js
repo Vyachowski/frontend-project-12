b18bb10947c6d2cceb807d744e25842fecf26b93
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-02-29 13:06:36 +0200

    feat: add 404 page with i18next

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,31 @@
+import { initReactI18next } from 'react-i18next';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import i18n from 'i18next';
 
 import SignupPage from './pages/SignupPage';
 import PageNotFound from './pages/404Page';
 import LoginPage from './pages/LoginPage';
 import MainPage from './pages/MainPage';
+import resources from './locales/index';
 import store from './store';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+i18n
+  .use(initReactI18next)
+  .init({
+    // the translations
+    // (tip move them in a JSON file and import them,
+    // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
+    resources,
+    lng: 'ru', // if you're using a language detector, do not define the lng option
+    fallbackLng: 'ru',
+
+    interpolation: {
+      escapeValue: false, // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
+    },
+  });
 
 const App = () => (
   <Provider store={store}>
--- frontend/src/pages/404Page.js
+++ frontend/src/pages/404Page.js
@@ -1,5 +1,26 @@
-const PageNotFound = () => (
-  <div>No such page</div>
-);
+import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
+
+import Header from '../components/Header';
+
+const PageNotFound = () => {
+  const { t } = useTranslation();
+
+  return (
+    <div className="d-flex flex-column h-100">
+      <Header />
+      <div className="container d-flex flex-column h-100 justify-content-center align-items-center">
+        <p>{t('page404.infoText')}</p>
+        <p>
+          {t('page404.redirectText')}
+          {' '}
+          <Link to="./">
+            {t('page404.linkRedirectText')}
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default PageNotFound;
