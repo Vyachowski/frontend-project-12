3503f06b4c11622b1915bc91eff6bda181de7087
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-02-29 13:25:15 +0200

    feat: add locales

---
+++ frontend/src/locales/ru.js
@@ -0,0 +1,9 @@
+export default {
+  translation: {
+    page404: {
+      infoText: 'Страница не найдена',
+      redirectText: 'Но вы можете перейти',
+      linkRedirectText: 'на главную страницу',
+    },
+  },
+};
