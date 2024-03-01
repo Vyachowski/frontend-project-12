05264eb09b40b947a78027db65c5c8ade5dc249d
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-03-01 12:31:04 +0200

    feat: add rollbar error handler

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 import { initReactI18next } from 'react-i18next';
 import { Provider } from 'react-redux';
 import i18n from 'i18next';
@@ -9,7 +10,7 @@ import LoginPage from './pages/LoginPage';
 import MainPage from './pages/MainPage';
 import resources from './locales/index';
 import store from './store';
-// eslint-disable-next-line
+
 import 'react-toastify/dist/ReactToastify.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -28,17 +29,25 @@ i18n
     },
   });
 
+const rollbarConfig = {
+  accessToken: '6585e2c2acda46f3b6ae4dd072016d81',
+};
+
 const App = () => (
-  <Provider store={store}>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/*" element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
-  </Provider>
+  <RollbarProvider config={rollbarConfig}>
+    <ErrorBoundary>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignupPage />} />
+            <Route path="/*" element={<PageNotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </Provider>
+    </ErrorBoundary>
+  </RollbarProvider>
 );
 
 export default App;
